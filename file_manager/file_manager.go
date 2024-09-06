377386284f9fd080e3bbbed848af468634eef8e6
ahartfelder <ahartfelder@gmail.com>
2024-09-06 11:38:25 -0300

    concurrency

--- file_manager/file_manager.go
+++ file_manager/file_manager.go
@@ -5,14 +5,22 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
-func ReadLines(path string) ([]string, error) {
-	file, err := os.Open(path)
+type FileManager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (fm FileManager) ReadLines() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
 
 	if err != nil {
 		return []string{}, errors.New("unable to open file")
 	}
+
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	var lines []string
@@ -24,29 +32,36 @@ func ReadLines(path string) ([]string, error) {
 	err = scanner.Err()
 
 	if err != nil {
-		file.Close()
 		return []string{}, errors.New("unable to read file")
 	}
 
-	file.Close()
 	return lines, nil
 }
 
-func WriteJSON(path string, data any) error {
-	file, err := os.Create(path)
+func (fm FileManager) WriteResult(data any) error {
+	file, err := os.Create(fm.OutputFilePath)
 
 	if err != nil {
 		return errors.New("unable to create file")
 	}
 
+	defer file.Close()
+
+	time.Sleep(3 * time.Second)
+
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
 		return errors.New("unable to convert data to JSON")
 	}
 
-	file.Close()
 	return nil
+}
+
+func New(inputPath, outputPath string) FileManager {
+	return FileManager{
+		inputPath,
+		outputPath,
+	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -4,42 +4,48 @@ import (
 	"fmt"
 
 	"example.com/price-calculator/conversion"
-	filemanager "example.com/price-calculator/file_manager"
+	"example.com/price-calculator/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	TaxRate           float64
-	InputPrices       []float64
-	TaxIncludedPrices map[string]string
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   iom,
 		TaxRate:     taxRate,
 		InputPrices: []float64{10, 20, 30},
 	}
 }
 
-func (job *TaxIncludedPriceJob) LoadData() {
-	lines, err := filemanager.ReadLines("prices.txt")
+func (job *TaxIncludedPriceJob) LoadData() error {
+	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	prices, err := conversion.StringsToFloat(lines)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	job.InputPrices = prices
+	return nil
 }
 
-func (job TaxIncludedPriceJob) Process() {
-	job.LoadData()
+func (job TaxIncludedPriceJob) Process(doneChan chan bool, errorChan chan error) {
+	err := job.LoadData()
+
+	if err != nil {
+		errorChan <- err
+		return
+	}
 
 	result := make(map[string]string)
 
@@ -49,5 +55,6 @@ func (job TaxIncludedPriceJob) Process() {
 	}
 
 	job.TaxIncludedPrices = result
-	filemanager.WriteJSON(fmt.Sprintf("result_%v.json", job.TaxRate), job)
+	job.IOManager.WriteResult(job)
+	doneChan <- true
 }
